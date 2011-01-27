cc157c4c27ebb74a4000f0ecafdac7b0be73e03f
Craig Buchek + Matt Simpson <t1d+cb+ms@asolutions.com>
2011-01-27 11:48:30 -0600

    Move comments out to a README file

--- switchgituser.sh
+++ switchgituser.sh
@@ -1,18 +1,4 @@
 #!/bin/bash
-# This script is very useful when you have a team of developers on a
-# project pushing to multiple remote sources
-#
-# Usage:
-#
-#   $ switchgituser.sh 
-#   1) matt.simpson		 3) kenny.ortmann	  5) arumugam.deivanayaga
-#   2) amos.king		 4) lori.cross		  6) craig.buchek
-#   #? 1
-#
-# When a user is selected, the .git/config file gets changed to reflect 
-# the selected user.
-#
-
 developers="matt.simpson amos.king kenny.ortmann lori.cross arumugam.deivanayaga craig.buchek"
 declare -A github
 github[matt.simpson]='coffeencoke'
